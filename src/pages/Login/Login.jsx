b7488f9d6d9b9da2b43e336460a861cbfae5e7b6
nassif-7 <nassif.adeip@gmail.com>
2023-08-19 21:30:35 +0300

    new login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
-import { Link } from 'react-router-dom';
+import { Link, redirect } from 'react-router-dom';
+import { fetchUser, login } from '../../redux/user/userActions';
+import { connect } from 'react-redux';
 
-function Login() {
+function Login({ login }) {
+  const [credentials, setCredentials] = useState({ userName: '', password: '' });
+
+  function handleInputChange(event) {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  }
+
+  function handleLogin(event) {
+    event.preventDefault();
+    login(credentials);
+    fetchUser();
+    redirect('/');
+  }
+
   return (
     <main className='login flex'>
       <div className='flex-shrink-0 login__banar'>
         <img src='/images/logo.png' alt='Best Aqua Solution' />
       </div>
       <div className='login__container'>
-        <form className='login__form'>
+        <form className='login__form' onSubmit={handleLogin}>
           <span className='login__form__subheader'>مرحباً مجدداً</span>
           <span className='login__form__header'>تسجيل الدخول</span>
 
           <div>
-            <input type='email' placeholder='البريد الالكتروني' name='' id='' className='login__form__input' />
+            <input
+              type='text'
+              placeholder='البريد الالكتروني'
+              name='userName'
+              autoComplete='true'
+              className='login__form__input'
+              onChange={handleInputChange}
+            />
           </div>
           <div>
-            <input type='password' placeholder='كلمة السر' name='' id='' className='login__form__input' />
+            <input
+              type='password'
+              placeholder='كلمة السر'
+              name='password'
+              autoComplete='true'
+              className='login__form__input'
+              onChange={handleInputChange}
+            />
           </div>
 
           <button type='submit' className='login__form__btn'>
@@ -25,8 +54,7 @@ function Login() {
           </button>
 
           <div className='login__form__link'>
-            تواجة مشكلة؟
-            <Link>طلب تغيير كلمة السر</Link>
+            تواجة مشكلة؟ <Link>طلب تغيير كلمة السر</Link>
           </div>
         </form>
       </div>
@@ -34,4 +62,15 @@ function Login() {
   );
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: (credentials) => dispatch(login(credentials)),
+    fetchUser: () => dispatch(fetchUser())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
